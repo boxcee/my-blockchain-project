b45f3c84efd7f7fd506869fd02c829dfb0bdae9c
Moritz Schmitz von Hülst <msc@marketlogicsoftware.com>
2018-12-02 23:57:15 +0100

    add some basic tests

--- test/ChallengeToken.js
+++ test/ChallengeToken.js
@@ -1,19 +1,37 @@
 const ChallengeToken = artifacts.require('./ChallengeToken.sol');
+const WhiteList = artifacts.require('./Whitelist.sol');
 
 contract('ChallengeToken', (accounts) => {
-  it('should create random number out of address', async () => {
+  it('should be readable', async () => {
     const contract = await ChallengeToken.deployed();
 
-    // Set myString to "Hey there!"
     const totalSupply = await contract.totalSupply.call();
     const name = await contract.name.call();
     const decimals = await contract.decimals.call();
     const symbol = await contract.symbol.call();
-    const balances = await contract.ballances.call();
     assert.equal('1000000', totalSupply.toString());
     assert.equal('ChallengeToken', name);
     assert.equal('8', decimals.toString());
     assert.equal('CHT', symbol);
-    assert.equal('CHT', balances);
+  });
+
+  it('should give all initial tokens to creator', async () => {
+    const contract = await ChallengeToken.deployed();
+
+    const balance = await contract.balanceOf.call(accounts[0]);
+    const totalSupply = await contract.totalSupply.call();
+    assert.equal(balance.toString(), totalSupply.toString());
+  });
+
+  it('should allow to transfer tokens', async () => {
+    // arrange
+    const contract = await ChallengeToken.deployed();
+    const whitelist = await WhiteList.deployed();
+    whitelist.addToWhitelist.sendTransaction(accounts[1], { from: accounts[0] });
+
+    // act
+    await contract.transfer.sendTransaction(accounts[1], 500, { from: accounts[0] });
+    const balanceOf = await contract.balanceOf.call(accounts[1]);
+    assert.equal('500', balanceOf.toString());
   });
 });
